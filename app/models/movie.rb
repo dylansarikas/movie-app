da4e4392f517cb5b4031b23a33ca36372e9c7bb7
dylan <dylan.sarikas@gmail.com>
2022-01-30 15:40:06 -0600

    adds Genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,8 @@ class Movie < ApplicationRecord
   validates :year, numericality: { only_integer: true }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+  
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,41 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-actor.save
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
 
-actor1 = Actor.create({first_name: "Butterfree", last_name: "Catinahat", known_for: "The Imitation Game"})
+# actor1 = Actor.create({first_name: "Butterfree", last_name: "Catinahat", known_for: "The Imitation Game"})
 
-actor2 = Actor.create({first_name: "Buffalo", last_name: "Custardpath", known_for: "Star Trek"})
+# actor2 = Actor.create({first_name: "Buffalo", last_name: "Custardpath", known_for: "Star Trek"})
 
-actor3 = Actor.create({first_name: "Benadryl", last_name: "Slumbercatch", known_for: "August: Osage County"})
+# actor3 = Actor.create({first_name: "Benadryl", last_name: "Slumbercatch", known_for: "August: Osage County"})
 
-actor4 = Actor.create({first_name: "Benedoctor", last_name: "Strangebatch", known_for: "Doctor Strange"})
+# actor4 = Actor.create({first_name: "Benedoctor", last_name: "Strangebatch", known_for: "Doctor Strange"})
 
-actor.save
+# actor.save
 
-10.times do
-  Actor.create({first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title})
-end
+# 10.times do
+#   Actor.create({first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title})
+# end
 
-movie1 = Movie.create({title: "The Imitation Game", year: 2014, plot: "During World War II, the English mathematical genius Alan Turing tries to crack the German Enigma code with help from fellow mathematicians while attempting to come to terms with his troubled private life."})
+# movie1 = Movie.create({title: "The Imitation Game", year: 2014, plot: "During World War II, the English mathematical genius Alan Turing tries to crack the German Enigma code with help from fellow mathematicians while attempting to come to terms with his troubled private life."})
 
-movie2 = Movie.create({title: "Star Trek Into Darkness", year: 2013, plot: "After the crew of the Enterprise find an unstoppable force of terror from within their own organization, Captain Kirk leads a manhunt to a war-zone world to capture a one-man weapon of mass destruction."})
+# movie2 = Movie.create({title: "Star Trek Into Darkness", year: 2013, plot: "After the crew of the Enterprise find an unstoppable force of terror from within their own organization, Captain Kirk leads a manhunt to a war-zone world to capture a one-man weapon of mass destruction."})
 
-movie3 = Movie.create({title: "August: Osage County", year: 2013, plot: "A look at the lives of the strong-willed women of the Weston family, whose paths have diverged until a family crisis brings them back to the Oklahoma house they grew up in, and to the dysfunctional woman who raised them."})
+# movie3 = Movie.create({title: "August: Osage County", year: 2013, plot: "A look at the lives of the strong-willed women of the Weston family, whose paths have diverged until a family crisis brings them back to the Oklahoma house they grew up in, and to the dysfunctional woman who raised them."})
 
-movie4 = Movie.create({title: "Doctor Strange", year: 2016, plot: "While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts."})
+# movie4 = Movie.create({title: "Doctor Strange", year: 2016, plot: "While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts."})
+
+# Genre.create(name: "Horror")
+# Genre.create(name: "Action")
+# Genre.create(name: "Adventure")
+# Genre.create(name: "Comedy")
+# Genre.create(name: "Romance")
+
+# MovieGenre.create(genre_id: 1, movie_id: 1)
+# MovieGenre.create(genre_id: 3, movie_id: 2)
+# MovieGenre.create(genre_id: 4, movie_id: 3)
+# MovieGenre.create(genre_id: 3, movie_id: 4)
+# MovieGenre.create(genre_id: 5, movie_id: 5)
+# MovieGenre.create(genre_id: 2, movie_id: 7)
+MovieGenre.create(genre_id: 4, movie_id: 7)
